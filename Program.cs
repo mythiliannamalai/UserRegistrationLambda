73ca6dc53791288eed4fb23f5c3c83847d44992b
mythili annamalai <mythiliannamalai45@gmail.com>
2022-04-06 14:41:06 +0530

    First name

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationLambda
+{
+    public class UserRegistration
+    {       
+        public void Firstnamein()
+        {
+            Regex FirstNames = new Regex(@"^[A-Z][A-Za-z]{3,}$");
+            bool MethodName(string Firstname) => FirstNames.IsMatch(Firstname);
+            bool result = MethodName("Mythili");
+            if (result == true)
+            {
+                Console.WriteLine("Valid Naem");
+            }
+            else
+            {
+                Console.WriteLine("invalid Naem..");
+            }
+        }  
+        static void Main(string[]args)
+        {
+            UserRegistration userRegistration = new UserRegistration();
+            userRegistration.Firstnamein();
+        }
+    }    
+}
