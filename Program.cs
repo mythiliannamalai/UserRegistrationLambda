6405c2f741e9b5b15b46813050573aeeb62d9ebe
mythili annamalai <mythiliannamalai45@gmail.com>
2022-04-06 14:47:40 +0530

    Last name

--- Program.cs
+++ Program.cs
@@ -16,17 +16,32 @@ namespace UserRegistrationLambda
             bool result = MethodName("Mythili");
             if (result == true)
             {
-                Console.WriteLine("Valid Naem");
+                Console.WriteLine("Valid Name");
             }
             else
             {
-                Console.WriteLine("invalid Naem..");
+                Console.WriteLine("invalid Name..");
             }
-        }  
+        }
+        public void Lastenamein()
+        {
+            Regex LastNames = new Regex(@"^[A-z][A-Za-z]{3,}$");
+            bool MethodName(string Lastname) => LastNames.IsMatch(Lastname);
+            bool result = MethodName("Annamalai");
+            if (result == true)
+            {
+                Console.WriteLine("Valid Name");
+            }
+            else
+            {
+                Console.WriteLine("invalid Name.");
+            }
+        }
         static void Main(string[]args)
         {
             UserRegistration userRegistration = new UserRegistration();
             userRegistration.Firstnamein();
+            userRegistration.Lastenamein();
         }
     }    
 }
